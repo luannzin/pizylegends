ee6b6fb60828db3639e25d8d63b64eea76b7a31e
Luan Fabri <luandaniel966@gmail.com>
2022-09-28 11:59:36 -0300

    Add files via upload

---
+++ pages/champions/[id].js
@@ -0,0 +1,120 @@
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          id: "Aatrox",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+
+  return {
+    props: {
+      id: id,
+    },
+  };
+}
+
+import React from "react";
+import styles from "../../styles/Champion.module.scss";
+
+const champion = ({ id }) => {
+  const [data, setData] = React.useState(null);
+  const [desc, setDesc] = React.useState(-1);
+
+  const handleClick = ({ target }) => {
+    if (target.localName === "div") {
+    } else {
+      setDesc(
+        Array.prototype.indexOf.call(target.parentElement.children, target) - 1
+      );
+    }
+  };
+
+  const renderChampion = React.useCallback(async () => {
+    const resposta = await fetch(
+      `http://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion/${id}.json`
+    );
+
+    const json = await resposta.json();
+
+    console.log(json["data"][id]);
+    setData(json["data"][id]);
+  }, []);
+
+  React.useEffect(() => {
+    renderChampion();
+  }, []);
+
+  if (data !== null) {
+    return (
+      <div className={styles.container}>
+        <div
+          style={{
+            background: `url(https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_0.jpg)`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+          className={styles.background}
+        ></div>
+        <div className={styles.champion}>
+          <div className={styles.preview}>
+            <img
+              src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${id}_0.jpg`}
+              alt=""
+            />
+          </div>
+          <div className={styles.championInfo}>
+            <h1>{id}</h1>
+            <p>{data.title}</p>
+            <div className={styles.championSkills}>
+              <h2>Skills</h2>
+              <div onClick={handleClick} className={styles.championSkillsImage}>
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/passive/${data.passive.image.full}`}
+                  alt=""
+                />
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/spell/${data.spells[0]["image"]["full"]}`}
+                  alt=""
+                />
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/spell/${data.spells[1]["image"]["full"]}`}
+                  alt=""
+                />
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/spell/${data.spells[2]["image"]["full"]}`}
+                  alt=""
+                />
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/spell/${data.spells[3]["image"]["full"]}`}
+                  alt=""
+                />
+              </div>
+              <div className={styles.championSkillsDesc}>
+                <p className={styles.bold}>
+                  {desc > -1
+                    ? data["spells"][desc]["name"]
+                    : data["passive"]["name"]}
+                </p>
+                <p>
+                  {desc > -1
+                    ? data["spells"][desc]["description"]
+                    : data["passive"]["description"]}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default champion;
--- pages/champions/index.js
+++ pages/champions/index.js
@@ -1,32 +1,83 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "../../styles/Champions.module.scss";
 import Image from "next/image";
-import Aatrox from "../../components/images/aatroxLittle.png";
+import Link from "next/link";
 
 const index = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.allChampions}>Todos os Campeões</h1>
-      <div className={styles.champions}>
-        <div className={styles.champion}>
-          <Image
-            src={Aatrox}
-            quality={100}
-            layout={"fixed"}
-            width={120}
-            height={120}
-          />
-          <div className={styles.championInfo}>
-            <h1>Aatrox</h1>
-            <div className={styles.championDesc}>
-              <p>The Darkin Blade</p>
-              <p>Fighter</p>
-            </div>
-          </div>
+  const [data, setData] = React.useState([]);
+  const [maximum, setMaximum] = React.useState(-8);
+  const [reached, setReached] = React.useState(false);
+  const sentinel = React.useRef();
+
+  const renderAllChampions = React.useCallback(async () => {
+    const response = await fetch(
+      "https://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion.json"
+    );
+
+    const json = await response.json();
+    Object.entries(json["data"]).map((js) => {
+      setData((data) => [...data, js[1]]);
+    });
+  });
+
+  React.useEffect(() => {
+    renderAllChampions();
+  }, []);
+
+  React.useEffect(() => {
+    if (maximum > data.length) {
+      setReached(true);
+    } else {
+      const observer = new IntersectionObserver((entries) => {
+        if (entries.some((entry) => entry.isIntersecting)) {
+          clearTimeout(referencia);
+          const referencia = setTimeout(() => {
+            setMaximum((maximum) => maximum + 8);
+          }, 600);
+        }
+      });
+
+      observer.observe(sentinel.current);
+      return () => observer.disconnect();
+    }
+  });
+
+  if (data !== null) {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.allChampions}>Todos os Campeões</h1>
+        <div className={styles.champions}>
+          {data.slice(0, maximum).map((ch) => {
+            return (
+              <Link href={`/champions/${ch.id}`}>
+                <div className={styles.champion}>
+                  <Image
+                    src={`http://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/${ch.id}.png`}
+                    quality={100}
+                    layout={"fixed"}
+                    width={120}
+                    height={120}
+                  />
+                  <div className={styles.championInfo}>
+                    <h1>{ch.id}</h1>
+                    <div className={styles.championDesc}>
+                      <p>{ch.title}</p>
+                      <p className={styles.championClass}>{ch["tags"][0]}</p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+          {!reached ? (
+            <div ref={sentinel} className={styles.sentinel}></div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default index;
